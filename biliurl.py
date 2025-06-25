efc1b2032a5163d3cc376b67282124717b8fb26e
zymooll <zymooll@qq.com>
2025-06-26 01:40:37 +0800

    init

---
+++ biliurl.py
@@ -0,0 +1,19 @@
+import requests
+import urllib
+import json
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+    'Accept-Language': 'zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3',
+    'Accept-Encoding': 'gzip, deflate',
+    'Connection': 'keep-alive',
+    'Upgrade-Insecure-Requests': '1',
+    'Cache-Control': 'max-age=0'
+}
+
+bvid='BV1kK4y1Y7Zq'
+resp=requests.get('https://api.bilibili.com/x/player/pagelist?bvid='+bvid,headers=headers)
+plist=resp.json()
+cid=plist.get('data')[0]['cid']
+
